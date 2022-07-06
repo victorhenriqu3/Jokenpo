1d874ba2de19e33a41475537b5760e2fa02e48b2
Victor Henrique <victorhenriqu3@outlook.com>
2022-07-06 01:08:53 -0400

    Criando a lógica básica de Vencedor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,59 @@
-console.log("Jokenpô Iniciou com sucesso ....");
-console.log("Jokenpô Iniciou com sucesso ....");
-console.log("Vamos começar ?");
+let acceptedChoices = ["Rock", "Paper", "Scissors"];
+
+let playerChoice = "";
+let computerChoice = "";
+let Winner = -1;
+let scorePlayer = 0;
+let scoreComputer = 0;
+let scoreTie = 0;
+
+function checkScore(win) {
+  const possibleWinners = {
+    0: () => scoreTie++,
+    1: () => scorePlayer++,
+    2: () => scoreComputer++,
+  };
+  possibleWinners[win]();
+  document.getElementById("Points-player").innerText = `${scorePlayer}`;
+  document.getElementById("Points-computer").innerText = `${scoreComputer}`;
+}
+
+function checkWinner(player, computer) {
+  const possibleChoices = {
+    Rock: computer === "Paper" ? 2 : 1,
+    Paper: computer === "Scissors" ? 2 : 1,
+    Scissors: computer === "Rock" ? 2 : 1,
+  };
+  return possibleChoices[player];
+}
+
+function selectChoices() {
+  acceptedChoices.forEach((item) => {
+    document
+      .getElementById(`choice-player-${item}`)
+      .classList.remove("selected");
+    document
+      .getElementById(`choice-computer-${item}`)
+      .classList.remove("selected");
+  });
+
+  document
+    .getElementById(`choice-player-${playerChoice}`)
+    .classList.add("selected");
+
+  document
+    .getElementById(`choice-computer-${computerChoice}`)
+    .classList.add("selected");
+}
+
+function plays(choice) {
+  playerChoice = choice;
+  computerChoice = acceptedChoices[Math.floor(Math.random() * (3 - 1) + 1)];
+  console.log(playerChoice, computerChoice);
+  Winner =
+    playerChoice === computerChoice ? 0 : checkWinner(choice, computerChoice);
+  console.log(Winner);
+
+  selectChoices();
+  checkScore(Winner);
+}
